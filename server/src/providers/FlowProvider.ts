36d718241a526da0f2c143c6a03b6bbab6a082e2
jasonycin <jason.lapie@gmail.com>
2023-02-27 14:04:18 -0500

    Add python requirements.txt file and set endpoint for Flow to use .env

--- server/src/providers/FlowProvider.ts
+++ server/src/providers/FlowProvider.ts
@@ -1,8 +1,13 @@
 import {Injectable} from "@tsed/di";
+import * as process from "process";
 
 @Injectable()
 export class FlowService {
-    private readonly endpoint: string = "https://api.flow.ai/v1";
+    private readonly endpoint = process.env['PURCHASE_FLOW_URL'] as string; // Casting verified in $onInit hook
+
+    async $onInit(): Promise<void> {
+        if (!this.endpoint) throw new Error('PURCHASE_FLOW_URL is not defined in .env file');
+    }
 
     async send() {
         return "Hello World!"
